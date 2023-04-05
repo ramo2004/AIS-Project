5cfbe39b84c22ae927308de7fe59b3ef783ccdde
ramdiego <127053578+ramdiego@users.noreply.github.com>
2023-04-05 12:14:04 -0500

    Add files via upload

---
+++ SkillSwap.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class SkillSwap {
+  
+  
+  public static void main(String[] args) { 
+    Scanner scnr = new Scanner(System.in);
+    
+    System.out.println("\nWelcome to SkillSwap!");
+    System.out.print("Enter your full name: ");
+    
+    String firstName = scnr.next();
+    String lastName = scnr.next();
+    
+    System.out.println("What are you looking to learn today " + );
+    
+  }
+  
+  /* ADD YOUR CODE HERE */
+  
+}
